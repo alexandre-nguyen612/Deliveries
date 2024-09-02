67ba6791d326a4f285e509079e90e416a1753569
alexandre-nguyen612 <72008493+alexandre-nguyen612@users.noreply.github.com>
2024-09-02 11:59:02 +0200

    Update ex_02.js

--- Day_01/ex_02.js
+++ Day_01/ex_02.js
@@ -1,2 +1,3 @@
 console.log("Hello World!"); /// Afficher le message dans la console
 alert("Hello World!") /// Afficher le message en popup
+document.write("Hello World!") ///Afficher le message dans la fenêtre
