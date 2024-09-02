7bb8e51de9ca01657427007c823216226cda1715
alexandre-nguyen612 <72008493+alexandre-nguyen612@users.noreply.github.com>
2024-09-02 14:14:29 +0200

    Upload ex_02

--- Day_01/ex_02.js
+++ Day_01/ex_02.js
@@ -1,3 +1,1 @@
-console.log("Hello World!"); /// Afficher le message dans la console
-alert("Hello World!") /// Afficher le message en popup
-document.write("Hello World!") ///Afficher le message dans la fenêtre
+document.write("Hello World!")
