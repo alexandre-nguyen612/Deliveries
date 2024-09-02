38a5f0e8daf38e54f9e3e5a5a81e3ebb1b115d11
alexandre-nguyen612 <72008493+alexandre-nguyen612@users.noreply.github.com>
2024-09-02 12:01:25 +0200

    Update ex_04.js

--- Day_01/ex_04.js
+++ Day_01/ex_04.js
@@ -1,2 +1,2 @@
-displayThisText("Welcome to the javascript pool! Have some fun and dont forget to bring a towel.");
+document.write("Welcome to the javascript pool! Have some fun and dont forget to bring a towel.");
 console.log("Website launched!")
