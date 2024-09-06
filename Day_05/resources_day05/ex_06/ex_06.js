f425221ebd211b5146adc27dee057915ff2f863d
alexandre-nguyen612 <72008493+alexandre-nguyen612@users.noreply.github.com>
2024-09-06 10:29:40 +0200

    Update ex_06.js du day05

--- Day_05/resources_day05/ex_06/ex_06.js
+++ Day_05/resources_day05/ex_06/ex_06.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('resize', function() {
         alert('Size matters');
+        document.querySelectorAll('img').forEach(function(image) {
+            image.addEventListener('mouseover', function() {
+                alert('Pretty, isn\'t it?');
+            });
+        });
     });
 });
